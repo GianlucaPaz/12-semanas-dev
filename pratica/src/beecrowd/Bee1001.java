8a085b598d202e2493bdc22346534601342ae3bc
Gianluca Paz <gianlucapaz@gmail.com>
2026-07-30 09:45:38 -0300

    feat(beecrowd): resolver 1001

---
+++ pratica/src/beecrowd/Bee1001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Bee1001 {
+    public static void main(String[] args) {
+
+        //Exercício BEE 1001 - Extremamente Básico
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int X = A + B;
+
+        System.out.println("X = " + X);
+    }
+}
