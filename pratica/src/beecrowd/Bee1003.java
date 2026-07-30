b3138ac837e7115f19ec6e473a3fe030492c66c8
Gianluca Paz <gianlucapaz@gmail.com>
2026-07-30 09:46:05 -0300

    feat(beecrowd): resolver 1003

---
+++ pratica/src/beecrowd/Bee1003.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Bee1003 {
+    public static void main(String[] args) {
+
+        //Exercício BEE 1003 - Soma Simples
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int SOMA = A + B;
+
+        System.out.println("SOMA = " + SOMA);
+    }
+}
