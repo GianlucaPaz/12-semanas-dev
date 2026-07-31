bfcbb3baafb3849d20e88c9dbb7964940b592e2b
Gianluca Paz <gianlucapaz@gmail.com>
2026-07-31 15:14:29 -0300

    feat(javanauta): resolver quiz de estruturas sequenciais

---
+++ pratica/src/javanauta/QuizEstruturasSequenciais.java
@@ -0,0 +1,56 @@
+public class QuizEstruturasSequenciais {
+    public static void main(String[] args) {
+
+        //Quiz Javanauta - Estruturas Sequenciais
+
+        //1) Qual é a saída do seguinte código?
+
+        int a = 10;
+        int b = 5;
+        int soma = a + b;
+        System.out.println("A soma é: " + soma);
+
+        //Resposta: A soma é: 15
+
+        /*---------------------------------------*/
+
+        //2) Qual operador é usado para concatenar strings em Java?
+
+        //Resposta: +
+
+        /*---------------------------------------*/
+
+        //3) Qual é a saída do seguinte código?
+
+        int x = 10;
+        int y = 2;
+        int resultado = x / y;
+        System.out.println("Resultado: " + resultado);
+
+        //Resposta: Resultado: 5
+
+        /*---------------------------------------*/
+
+        //4) Como você declara e inicializa uma variável inteira com valor 25?
+
+        //Resposta: int numero = 25;
+
+        /*---------------------------------------*/
+
+        //5) Qual é a saída do seguinte código?
+
+        System.out.println("Resultado: " + (3 * 5));
+
+        //Resposta: Resultado 15
+
+        /*---------------------------------------*/
+
+        //6) Qual metodo da classe Scanner é usado para ler uma string do teclado?
+
+        //Resposta: nextLine()
+
+        /*---------------------------------------*/
+
+        //RESULTADO DO QUIZ --> Acertou 100% das questões!
+    }
+}
