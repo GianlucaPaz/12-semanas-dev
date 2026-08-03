a53c4bb4b40144ffb2eaf03b76554e3debacd59a
Gianluca Paz <gianlucapaz@gmail.com>
2026-08-03 15:49:13 -0300

    feat(beecrowd): resolver 1011

---
+++ pratica/src/beecrowd/Bee1011.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Bee1011 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        System.out.print("Digite o valor do raio da esfera: ");
+        double R = scanner.nextDouble();
+        double pi = 3.14159;
+
+        double VOLUME = (4 * pi * Math.pow(R,3)) / 3;
+        System.out.printf(Locale.US, "VOLUME = %.3f%n", VOLUME);
+    }
+}
