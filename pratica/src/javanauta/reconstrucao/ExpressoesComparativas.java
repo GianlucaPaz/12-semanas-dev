e04c669c5626378aed58d14c84efa25664fce38d
Gianluca Paz <gianlucapaz@gmail.com>
2026-08-03 16:01:39 -0300

    feat(javanauta): reconstruir expressões comparativas

---
+++ pratica/src/javanauta/reconstrucao/ExpressoesComparativas.java
@@ -0,0 +1,48 @@
+package reconstrucao;
+
+public class ExpressoesComparativas {
+    public static void main(String[] args) {
+
+        //Reconstrução Javanauta - Expressões Comparativas
+
+        //Expressão Comparativa de igualdade (==)
+        int valor1 = 10;
+        int valor2 = 20;
+
+        System.out.println("--------------------------------------------------");
+        System.out.println("1) O valor1 (10) é igual ao valor2 (20) ?");
+        System.out.print("Resposta: ");
+        System.out.println(valor1 == valor2);
+        System.out.println("--------------------------------------------------");
+
+        //Expressão Comparativa de diferença (!=)
+        System.out.println("2) O valor1 (10) é diferente do valor2 (20) ?");
+        System.out.print("Resposta: ");
+        System.out.println(valor1 != valor2);
+        System.out.println("--------------------------------------------------");
+
+        //Expressão Comparativa de maior que (>)
+        System.out.println("3) O valor1 (10) é maior que valor2 (20) ?");
+        System.out.print("Resposta: ");
+        System.out.println(valor1 > valor2);
+        System.out.println("--------------------------------------------------");
+
+        //Expressão Comparativa de menor que (<)
+        System.out.println("4) O valor1 (10) é menor que valor2 (20) ?");
+        System.out.print("Resposta: ");
+        System.out.println(valor1 < valor2);
+        System.out.println("--------------------------------------------------");
+
+        //Expressão Comparativa de maior ou igual que (>=)
+        System.out.println("5) O valor1 (10) é maior ou igual que o valor2 (20) ?");
+        System.out.print("Resposta: ");
+        System.out.println(valor1 >= valor2);
+        System.out.println("--------------------------------------------------");
+
+        //Expressão Comparativa de menor ou igual que (<=)
+        System.out.println("6) O valor1 (10) é menor ou igual que o valor2 (20) ?");
+        System.out.print("Resposta: ");
+        System.out.println(valor1 <= valor2);
+        System.out.println("--------------------------------------------------");
+    }
+}
