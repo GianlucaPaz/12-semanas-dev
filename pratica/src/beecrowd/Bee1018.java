d0a2d5b2de6a7f0ddd3f2d30c4cac20257e1114c
Gianluca Paz <gianlucapaz@gmail.com>
2026-08-05 21:53:52 -0300

    feat(beecrowd): resolver 1018

---
+++ pratica/src/beecrowd/Bee1018.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bee1018 {
+    public static void main(String[] args) {
+
+        //Exercício BEE 1018 - Cédulas
+
+        Scanner scanner = new Scanner(System.in);
+
+        //???????????????
+    }
+}
