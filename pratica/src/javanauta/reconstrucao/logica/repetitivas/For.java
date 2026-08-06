eff147eec902027993c213fbbe49da9285edf4b8
Gianluca Paz <gianlucapaz@gmail.com>
2026-08-06 18:47:31 -0300

    feat(javanauta): reconstruir for

---
+++ pratica/src/javanauta/reconstrucao/logica/repetitivas/For.java
@@ -0,0 +1,13 @@
+package reconstrucao.logica.repetitivas;
+
+public class For {
+    public static void main(String[] args) {
+
+        //Reconstrução Javanauta - For
+
+        //--> Reconstrução com For (bem simples)
+        for (int cont = 1; cont < 10; cont++) {
+            System.out.println("Repetição número " + cont + "!");
+        }
+    }
+}
