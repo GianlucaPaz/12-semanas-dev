d0be5196ee5d08c1f8dbc4881a92e6d873f05f47
Gianluca Paz <gianlucapaz@gmail.com>
2026-08-26 22:01:38 -0300

    feat(javanauta): reconstruir metodos com o que faltava

--- pratica/src/javanauta/reconstrucao/poo/Metodos.java
+++ pratica/src/javanauta/reconstrucao/poo/Metodos.java
@@ -5,29 +5,59 @@ import exercicios.RevisaoLogicaExercicio5;
 
 public class Metodos {
 
-    //Reconstrução Javanauta - Modificadores de Acesso, Tipos de Retorno e Como nomear Métodos
+    //Reconstrução Javanauta - Modificadores de Acesso, Tipos de Retorno, Como nomear Métodos, Parâmetros de Métodos, Métodos de Instância e Métodos Estáticos
 
-    //-> Exemplo com tipo de retorno int
+    //-> A Classe
 
-    protected int a;
-    protected int b;
-    protected int c;
+    //Atributos
+    private int a;
+    private int b;
+    private int c;
 
+    //Métodos
 
-    protected int somarValores() {
+    //Método1: Retorna a soma de 3 números inteiros
+    private int somarValores() {
         return a + b + c;
     }
 
+    //Método2: Converte o parâmetro para double
+    private double converterParaDouble(int valor) {
+        return valor;   // O Java converte para double automaticamente neste momento
+    }
+
+    /*==============================================================================================================*/
+
+    //-> Suposta classe de teste
+
     public static void main(String[] args) {
-        Metodos somatorioDeValores = new Metodos();
 
-        somatorioDeValores.a = 1;
-        somatorioDeValores.b = 2;
-        somatorioDeValores.c = 4;
+        //-> Criação do Objeto calculo ao instanciar a classe Metodos
+        Metodos calculo = new Metodos();
 
-        int resultado = somatorioDeValores.somarValores();
-        System.out.println("====================================");
-        System.out.println("A soma dos valores de a, b e c é: " + resultado);
-        System.out.println("====================================");
+        //Atribuição das variáveis (atributos da classe)
+        calculo.a = 1;
+        calculo.b = 2;
+        calculo.c = 4;
+
+        //Chamada do Método1 (de instância)
+        int resultado = calculo.somarValores();
+
+        System.out.println("==================================================================");
+        System.out.println("-> Método1 (de instância)");
+        System.out.println("   - A soma dos valores de a, b e c é: " + resultado);
+        System.out.println("==================================================================");
+
+        //Chamada do Método2 (de instância)
+        double valorConvertido = calculo.converterParaDouble(resultado);
+
+        System.out.println("==================================================================");
+        System.out.println("-> Método2 (de instância)");
+        System.out.printf("   - Valor %d convertido para double é = %.2f%n", resultado, valorConvertido);
+        System.out.println("==================================================================");
+
+        //-> Chamada do Método3 (Estático) dentro print
+        System.out.println("-> Método3 (estático)");
+        System.out.println("   - A resposta da multiplicação é = " + MetodoEstatico.multiplicarValores());
     }
 }
